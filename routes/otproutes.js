5cf0540da916a036613cf359b7e63a65adb3e117
Manashvi Sahani <manashvisahani@gmail.com>
2025-08-20 15:23:03 +0530

    fixed middleware otproutes

--- routes/otproutes.js
+++ routes/otproutes.js
@@ -3,24 +3,33 @@ const express = require('express');
 const router = express.Router();
 const verify_user = require('../controller/otpHandler/verifyotp.js');   
 
-router.post('/email_verification',((req,res)=>{
-    req.body.purpose = "email_verification";
+router.post('/email_verification',((req,res,next)=>{
+    req.body.purpose = "email_verification";next();
 }), verify_user);
 
 router.post('/login',
-    ((req,res)=>{
-    req.body.purpose = "login";   
+    ((req,res,next)=>{
+    req.body.purpose = "login";  next(); 
 }), verify_user);
-router.post('/registration',((req,res)=>{
-    req.body.purpose = "registration";
 
-}), verify_user);
-router.post('/invite',((req,res)=>{
-    req.body.purpose = "invite";
 
+router.post('/registration',((req,res,next)=>{
+    req.body.purpose = "registration";next();
 }), verify_user);
-router.post('/password_reset',((req,res)=>{
-    req.body.purpose = "password_reset";
+router.post('/invite',((req,res,next)=>{
+    req.body.purpose = "invite";next();
+}), verify_user);
+
+router.post('/password_reset',((req,res,next)=>{
+    req.body.purpose = "password_reset";next();
 }), verify_user);
 
 module.exports = router
+
+
+//post
+//url/verifyotp/login
+//url/verifyotp/email_verification
+//url/verifyotp/registration
+//url/verifyotp/invite
+//url/verifyotp/password_reset
